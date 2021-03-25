97d8c08fbb6150ab5a49b24863c88a8a01dfb51b
Prakash Chater <53861867+Prakashchater@users.noreply.github.com>
2021-03-25 15:33:35 +0530

    update

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from selenium.common.exceptions import NoSuchElementException, ElementClickInter
 from selenium.webdriver.common.keys import Keys
 import time
 
-email = "iamprakash1609@gmail.com"
-password = "pcchater@1609"
-webdriver_path = "C:\Chrome driver\chromedriver.exe"
+email = "YOUR FACEBOOK EMAIL ID"
+password = "YOUR PASSWORD"
+webdriver_path = "YOUR WEB DRIVER FILE PATH"
 url = "https://tinder.com/"
 driver = webdriver.Chrome(executable_path=webdriver_path)
 driver.get(url)
@@ -66,7 +66,7 @@ for i in range(10):
         print("swiping")
         like_button = driver.find_element_by_xpath('//*[@id="t--771258051"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]')
         like_button.click()
-
+    #If any match will pop-up it will continue the swiping.
     except ElementClickInterceptedException:
         try:
             match_popup = driver.find_element_by_css_selector(".itsAMatch a")
@@ -74,11 +74,4 @@ for i in range(10):
 
         except NoSuchElementException:
             time.sleep(3)
-
-
-
-
-
-
-
-
+            
